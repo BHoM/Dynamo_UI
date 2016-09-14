41d34e3b7018674e22ac260406e351cf643b291e
adecler <arnaud.declercq@burohappold.com>
2016-09-14 14:40:56 +0100

    Adding motion level analysis of videos

---
+++ MachineLearning_Basilisk/VIdeoAnalysis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning
+{
+    public static class VideoAnalysis
+    {
+        public static Dictionary<int, double> MotionLevel(string videoFile, int startFrame = 0, int endFrame = int.MaxValue, string outFolder = "", bool trigger = false)
+        {
+            if (trigger)
+            {
+                MachineLearning_Engine.MotionLevelAnalyser analyser = new MachineLearning_Engine.MotionLevelAnalyser();
+                return analyser.Run(videoFile, startFrame, endFrame, outFolder).Result;
+            }
+            else
+                return null;
+        }
+    }
+}
