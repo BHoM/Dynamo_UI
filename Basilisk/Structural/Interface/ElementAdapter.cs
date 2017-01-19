c1c3480389e3bbc69eb0dbd6875b3eb3771d5006
BUROHAPPOLD\asater <ahmad.sater@BuroHappold.com>
2017-01-19 14:37:13 +0000

    Adding GetPanels node to Basilisk interface

--- Basilisk/Structural/Interface/ElementAdapter.cs
+++ Basilisk/Structural/Interface/ElementAdapter.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using BHE = BHoM.Structural.Elements;
 using BHL = BHoM.Structural.Loads;
 using BHI = BHoM.Structural.Interface;
+using Autodesk.DesignScript.Runtime;
+using DSG = Autodesk.DesignScript.Geometry;
+using Geometry;
 
 namespace Structural.Interface
 {
@@ -53,5 +56,39 @@ namespace Structural.Interface
                 ((BHI.IElementAdapter)app).SetPanels(panels, out ids);
             return ids;
         }
+
+        /// <param name="selection">All = 0, Selected = 1, FromInput = 2</param>
+        [MultiReturn(new[] { "Panels", "ids" })]
+        public static Dictionary<string, List<object>> GetPanels(object app, List<string> objectIds = null, int selection =0, bool activate = true)
+        {
+            string[] columnheaders = { "Panels", "ids" };
+            List<BHE.Panel> panels = null;
+            List < DSG.PolyCurve > geometry = null;
+            List<string> outIds = new List<string>();
+
+            Dictionary<string,List<object>> output = new Dictionary<string, List<object>>();
+
+            if (activate)
+            {
+                ((BHI.IElementAdapter)app).Selection = (BHI.ObjectSelection)selection;
+                outIds = ((BHI.IElementAdapter)app).GetPanels(out panels, objectIds);
+            }
+
+            List<object> o1 = new List<object>();
+            List<object> o2 = new List<object>();
+
+            for (int j = 0; j < panels.Count; j++)
+            {
+                o1.Add(panels[j]);
+                o2.Add(outIds[j]);
+            }
+
+            output[columnheaders[0]] = o1;
+            output[columnheaders[1]] = o2;
+
+
+            return output;
+            //return panels;
+        } 
     }
 }
