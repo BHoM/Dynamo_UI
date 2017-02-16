4367468b4f20baba3d2a4c53d429e68144995d45
oscarborgstrom <Oscar.Borgstrom@burohappold.com>
2017-02-16 16:34:13 +0000

    Copy and Icons
    Build projectfile with pkg.json. Now copying into Dynamo 1.2 package
    folder. Also startde addition of Icons and Environmental object
    creation.

---
+++ Basilisk/Environmental/Elements/BHoM_Panel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DSG = Autodesk.DesignScript.Geometry;
+using BHG = BHoM.Geometry;
+using BHE = BHoM.Environmental.Elements;
+
+namespace Environmental.Elements
+{
+
+    /// <summary>
+    /// Environmental tools
+    /// BuroHappold
+    /// <class name="BHGeometryTools">Geometry tools for Dynamo</class>
+    /// </summary>
+    public static class BHPanel
+    {
+        /// <summary></summary>
+        public static BHE.Panel FromPolyline(List<BHG.Line> Lines, Dictionary<string, object> CustomData = null, string Name = null)
+        {
+            BHE.Panel panel= new BHE.Panel();
+            panel.Lines = Lines;
+            panel.CustomData = CustomData;
+            panel.Name = Name;
+            return panel;
+        }
+    }
+}
---
+++ Basilisk/Environmental/Elements/BHoM_Wall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DSG = Autodesk.DesignScript.Geometry;
+using BHG = BHoM.Geometry;
+using BHE = BHoM.Environmental.Elements;
+using DC = Dynamo.Core;
+
+namespace Environmental.Elements
+{
+
+    /// <summary>
+    /// Environmental tools
+    /// BuroHappold
+    /// <class name="BHGeometryTools">Geometry tools for Dynamo</class>
+    /// </summary>
+    public static class BHWall
+    {
+        /// <summary></summary>
+        public static BHE.Wall FromPolyline(BHG.Line Line, List<BHE.Panel> Panels = null, Dictionary<string, object> CustomData = null, string Name = null)
+        {
+            BHE.Wall wall = new BHE.Wall();
+            wall.Line = Line;
+            wall.CustomData = CustomData;
+            wall.Name = Name;
+            return wall;
+        }
+    }
+}
