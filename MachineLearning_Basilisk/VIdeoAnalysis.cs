a523e9ac4b5ca1f4f6f19456fa60e358d4d200d1
adecler <arnaud.declercq@burohappold.com>
2017-02-22 12:29:43 +0000

    video analysis component for video analysis

--- MachineLearning_Basilisk/VIdeoAnalysis.cs
+++ MachineLearning_Basilisk/VIdeoAnalysis.cs
@@ -8,12 +8,21 @@ namespace MachineLearning
 {
     public static class VideoAnalysis
     {
-        public static Dictionary<int, double> MotionLevel(string videoFile, int startFrame = 0, int endFrame = int.MaxValue, string outFolder = "", bool trigger = false)
+        public static Dictionary<int, List<double>> MotionLevel(string videoFile, int startFrame = 0, int endFrame = int.MaxValue, int frameStep = 1, int nbRows = 1, int nbColumns = 1, string outFolder = "", bool trigger = false)
         {
             if (trigger)
             {
                 MachineLearning_Engine.MotionLevelAnalyser analyser = new MachineLearning_Engine.MotionLevelAnalyser();
-                return analyser.Run(videoFile, startFrame, endFrame, outFolder).Result;
+
+                MachineLearning_Engine.MotionLevelAnalyser.Config config = new MachineLearning_Engine.MotionLevelAnalyser.Config();
+                config.StartFrame = startFrame;
+                config.EndFrame = endFrame;
+                config.FrameStep = frameStep;
+                config.NbRows = nbRows;
+                config.NbColumns = nbColumns;
+                config.OutFolder = outFolder;
+
+                return analyser.Run(videoFile, config).Result;
             }
             else
                 return null;
