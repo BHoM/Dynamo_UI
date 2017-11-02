7a8f437aeb5f8691af3b46e727c9a938777d539c
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2017-11-02 17:49:52 +0000

    Added the dynamo adapter project

---
+++ Dynamo_Adapter/CRUD/Create.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.DynamoBIM
+{
+    public partial class DynamoBIMAdapter
+    {
+        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dynamo_Adapter/CRUD/Pull.cs
@@ -0,0 +1,18 @@
+﻿using BH.Adapter.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.DynamoBIM
+{
+    public partial class DynamoBIMAdapter
+    {
+        public IEnumerable<object> Pull(IEnumerable<IQuery> query, Dictionary<string, string> config = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
