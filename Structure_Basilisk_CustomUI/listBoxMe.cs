e2dc9f16d954cc1c7ffc30a5c245e941aa85a045
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2017-11-10 17:51:42 +0000

    One step closer to the serialization

--- Structure_Basilisk_CustomUI/listBoxMe.cs
+++ Structure_Basilisk_CustomUI/listBoxMe.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 using ProtoCore.AST.AssociativeAST;
 using Dynamo.Graph;
 using System.Xml;
+using DynamoUnits;
 
 namespace Structure_Basilisk_CustomUI
 {
@@ -22,122 +23,24 @@ namespace Structure_Basilisk_CustomUI
     [NodeCategory("Basilisk.Base.BHoMObject")]
     [IsDesignScriptCompatible]
     [Serializable]
-    
 
-    public class listBoxMe : NodeModel, INodeViewCustomization<listBoxMe>
+    
+    public class listBoxMe : NodeModel
     {
-        private double _sliderValue;
+        private listBoxMeView _thisView;
+        public List<ParameterInfo> inputs { get; set; }
+        public ConstructorInfo ConstructorInfo { get; set; } = null;
+        public Type test = null;
+        public bool runView = true;
 
         public listBoxMe()
         {
             OutPortData.Add(new PortData("BHoM Object", "Your custom created BHoM object"));
-            RegisterAllPorts();
         }
 
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
         {
             return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), SetOutput(inputAstNodes)) };
-        }
-
-        private DynamoViewModel dynamoViewModel;
-        private listBoxMe model;
-        private NodeView view;
-        protected Type test = null;
-        protected ConstructorInfo m_Constructor = null;
-        protected List<MethodInfo> m_DaGets = new List<MethodInfo>();
-
-
-        public void CustomizeView(listBoxMe nodeModel, NodeView nodeView)
-        {
-            view = nodeView;
-            model = nodeModel;
-            MenuItem types = new MenuItem { Header = "BHoM Types", IsCheckable = false };
-            types = GetTypes(types);
-            nodeView.MainContextMenu.Items.Add(types);
-            types.Click += Types_Click;
-            nodeModel.InPorts.Clear();
-        }
-
-        private void Types_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            MenuItem test1 = e.OriginalSource as MenuItem;
-            test = (Type)test1.Tag;
-            GetInputInfo(test);
-        }
-
-        public void GetInputInfo(Type type)
-        {
-            ConstructorInfo[] constructors = type.GetConstructors();
-            m_Constructor = constructors[0];
-            foreach (ConstructorInfo info in constructors)
-            {
-                ParameterInfo[] param = info.GetParameters();
-                if (info.GetParameters().Length > m_Constructor.GetParameters().Length)
-                    m_Constructor = info;
-            }
-            List<ParameterInfo> inputs = m_Constructor.GetParameters().ToList();
-            UpdateInputs(inputs);
-            int items = view.MainContextMenu.Items.Count;
-            view.MainContextMenu.Items.RemoveAt(items - 1);
-            //view.UpdateLayout();
-
-        }
-
-        public void UpdateInputs(List<ParameterInfo> inputs)
-        {
-            foreach (ParameterInfo info in inputs)
-                model.InPortData.Add(new PortData(info.Name, "test"));
-
-            model.RegisterAllPorts();
-            model.NickName = "BHoM " + test.Name;
-            model.OverrideNameWithNickName = true;
-        }
-
-
-        public MenuItem GetTypes(MenuItem menuItem)
-        {
-            Type bhomType = typeof(BHoMObject);
-            IEnumerable<Type> types = types = BHER.Create.TypeList().Where(x => x.IsSubclassOf(bhomType) && !x.ContainsGenericParameters).OrderBy(x => x.Name);
-
-            foreach (Type type in types)
-            {
-                MenuItem tempItem = new MenuItem() { Header = type.Name, IsCheckable = false, Tag = type };
-                menuItem.Items.Add(tempItem);
-            }
-            return menuItem;
-        }
-
-        public void Dispose()
-        {
-        }
-
-        protected override void SerializeCore(XmlElement element, SaveContext context)
-        {
-            base.SerializeCore(element, context);
-
-            if (test != null && m_Constructor != null)
-            {
-                var serNode = element.OwnerDocument.CreateElement("TypeName");
-                serNode.InnerText = test.AssemblyQualifiedName;
-                element.AppendChild(serNode);
-            }           
-        }
-
-        protected override void DeserializeCore(XmlElement nodeElement, SaveContext context)
-        {
-            base.DeserializeCore(nodeElement, context);
-            var deSer = nodeElement.ChildNodes.Cast<XmlNode>().FirstOrDefault(x => x.Name == "TypeName");
-            test = Type.GetType(deSer.InnerText);
-
-            ConstructorInfo[] constructors = test.GetConstructors();
-            m_Constructor = constructors[0];
-            foreach (ConstructorInfo info in constructors)
-            {
-                ParameterInfo[] param = info.GetParameters();
-                if (info.GetParameters().Length > m_Constructor.GetParameters().Length)
-                    m_Constructor = info;
-            }
-            List<ParameterInfo> inputs = m_Constructor.GetParameters().ToList();
         }
 
         public AssociativeNode SetOutput(List<AssociativeNode> inputAstNodes)
@@ -148,7 +51,7 @@ namespace Structure_Basilisk_CustomUI
 
             try
             {
-                var function = AstFactory.BuildPrimitiveNodeFromObject(m_Constructor.Invoke(inputAstNodes.ToArray()));
+                var function = AstFactory.BuildPrimitiveNodeFromObject(ConstructorInfo.Invoke(inputAstNodes.ToArray()));
                 return function;
             }
             catch (Exception)
@@ -159,5 +62,50 @@ namespace Structure_Basilisk_CustomUI
             return AstFactory.BuildNullNode();
 
         }
+        protected override void SerializeCore(XmlElement element, SaveContext context)
+        {
+            base.SerializeCore(element, context);
+
+            if (test != null && ConstructorInfo != null)
+            {
+                var serNode = element.OwnerDocument.CreateElement("TypeName");
+                serNode.InnerText = test.AssemblyQualifiedName;
+                element.AppendChild(serNode);
+            }
+        }
+
+        protected override void DeserializeCore(XmlElement nodeElement, SaveContext context)
+        {
+            base.DeserializeCore(nodeElement, context);
+            var deSer = nodeElement.ChildNodes.Cast<XmlNode>().FirstOrDefault(x => x.Name == "TypeName");
+            test = Type.GetType(deSer.InnerText);
+            ConstructorInfo[] constructors = test.GetConstructors();
+            ConstructorInfo = constructors[0];
+
+            if (ConstructorInfo != null)
+                runView = false;
+
+            foreach (ConstructorInfo info in constructors)
+            {
+                ParameterInfo[] param = info.GetParameters();
+                if (info.GetParameters().Length > ConstructorInfo.GetParameters().Length)
+                    ConstructorInfo = info;
+            }
+            inputs = ConstructorInfo.GetParameters().ToList();
+            
+        }
+
+        //public void UpdateInputs(List<ParameterInfo> inputs)
+        //{
+        //    foreach (ParameterInfo info in inputs)
+        //        InPortData.Add(new PortData(info.Name, "test"));
+
+        //    RegisterAllPorts();
+        //    NickName = "BHoM " + test.Name;
+        //    OverrideNameWithNickName = true;
+        //}
+
+
+
     }
 }
--- Structure_Basilisk_CustomUI/listBoxNodeView.cs
+++ Structure_Basilisk_CustomUI/listBoxNodeView.cs
@@ -1,90 +1,103 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Windows.Controls;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Dynamo.Controls;
-//using Dynamo.Graph.Nodes;
-//using Dynamo.Wpf;
-//using Dynamo.ViewModels;
-//using BH.oM.Base;
-//using BHER = BH.Engine.Reflection;
-//using System.Reflection;
-//using ProtoCore.AST.AssociativeAST;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Controls;
+using Dynamo.Graph.Nodes;
+using Dynamo.Wpf;
+using Dynamo.ViewModels;
+using BH.oM.Base;
+using BHER = BH.Engine.Reflection;
+using System.Reflection;
+using ProtoCore.AST.AssociativeAST;
+using Dynamo.Graph;
+using System.Xml;
 
-//namespace Structure_Basilisk_CustomUI
-//{
-//    public class listBoxNodeView :  INodeViewCustomization<listBoxMe>
-//    {
-//        private DynamoViewModel dynamoViewModel;
-//        private listBoxMe model;
-//        private NodeView view;
-//        protected Type test;
+namespace Structure_Basilisk_CustomUI
+{
 
-
-//        public void CustomizeView(listBoxMe nodeModel, NodeView nodeView)
-//        {
-//            view = nodeView;
-//            model = nodeModel;
-//            MenuItem types = new MenuItem { Header = "BHoM Types", IsCheckable = false };
-//            types = GetTypes(types);
-//            nodeView.MainContextMenu.Items.Add(types);
-//            types.Click += Types_Click;
-//            nodeModel.InPorts.Clear();
-//        }
-
-//        private void Types_Click(object sender, System.Windows.RoutedEventArgs e)
-//        {
-//            MenuItem test1 = e.OriginalSource as MenuItem;
-//            test = (Type)test1.Tag;
-//            GetInputInfo(test);
-//        }
-
-//        public void GetInputInfo(Type type)
-//        {
-//            ConstructorInfo[] constructors = type.GetConstructors();
-//            ConstructorInfo Constructor = constructors[0];
-//            foreach (ConstructorInfo info in constructors)
-//            {
-//                ParameterInfo[] param = info.GetParameters();
-//                if (info.GetParameters().Length > Constructor.GetParameters().Length)
-//                    Constructor = info;
-//            }
-//            List<ParameterInfo> inputs = Constructor.GetParameters().ToList();
-//            UpdateInputs(inputs);
-//            int items = view.MainContextMenu.Items.Count;
-//            view.MainContextMenu.Items.RemoveAt(items - 1);
-//            view.UpdateLayout();
-
-//        }
-
-//        public void UpdateInputs(List<ParameterInfo> inputs)
-//        {
-//            foreach (ParameterInfo info in inputs)
-//                model.InPortData.Add(new PortData(info.Name, "test"));
-
-//            model.RegisterAllPorts();
-//            model.NickName = "BHoM " + test.Name;
-//            model.OverrideNameWithNickName = true;
-//        }
+    public class listBoxMeView : INodeViewCustomization<listBoxMe>
+    {
+        private DynamoViewModel dynamoViewModel;
+        private listBoxMe model;
+        private NodeView view;
 
 
-//        public MenuItem GetTypes(MenuItem menuItem)
-//        {
-//            Type bhomType = typeof(BHoMObject);
-//            IEnumerable<Type> types = types = BHER.Create.TypeList().Where(x => x.IsSubclassOf(bhomType) && !x.ContainsGenericParameters).OrderBy(x => x.Name);
+        public void CustomizeView(listBoxMe nodeModel, NodeView nodeView)
+        {
+            view = nodeView;
+            model = nodeModel;
+            if (model.runView)
+            {
+                MenuItem types = new MenuItem { Header = "BHoM Types", IsCheckable = false };
+                types = GetTypes(types);
+                nodeView.MainContextMenu.Items.Add(types);
+                types.Click += Types_Click;
+                model.RegisterAllPorts();
+            }
+            else
+            {
+                UpdateInputs(model.inputs);
+            }
+            //model.UpdateInputs(model.inputs);
+        }
 
-//            foreach (Type type in types)
-//            {
-//                MenuItem tempItem = new MenuItem() { Header = type.Name, IsCheckable = false, Tag = type};
-//                menuItem.Items.Add(tempItem);
-//            }
-//            return menuItem;
-//        }
+        public MenuItem GetTypes(MenuItem menuItem)
+        {
+            Type bhomType = typeof(BHoMObject);
+            IEnumerable<Type> types = types = BHER.Create.TypeList().Where(x => x.IsSubclassOf(bhomType) && !x.ContainsGenericParameters).OrderBy(x => x.Name);
 
-//            public void Dispose()
-//        {
-//        }
-//    }
-//}
+            foreach (Type type in types)
+            {
+                MenuItem tempItem = new MenuItem() { Header = type.Name, IsCheckable = false, Tag = type };
+                menuItem.Items.Add(tempItem);
+            }
+            return menuItem;
+        }
+
+        public void Dispose()
+        {
+        }
+
+
+        private void Types_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MenuItem test1 = e.OriginalSource as MenuItem;
+            model.test = (Type)test1.Tag;
+            GetInputInfo(model.test);
+        }
+
+
+
+        public void GetInputInfo(Type type)
+        {
+            ConstructorInfo[] constructors = type.GetConstructors();
+            model.ConstructorInfo = constructors[0];
+            foreach (ConstructorInfo info in constructors)
+            {
+                ParameterInfo[] param = info.GetParameters();
+                if (info.GetParameters().Length > model.ConstructorInfo.GetParameters().Length)
+                    model.ConstructorInfo = info;
+            }
+            model.inputs = model.ConstructorInfo.GetParameters().ToList();
+            UpdateInputs(model.inputs);
+            int items = view.MainContextMenu.Items.Count;
+            view.MainContextMenu.Items.RemoveAt(items - 1);
+            //view.UpdateLayout();
+            //model.ConstructorInfo = m_Constructor;
+        }
+
+        public void UpdateInputs(List<ParameterInfo> inputs)
+        {
+            foreach (ParameterInfo info in inputs)
+                model.InPortData.Add(new PortData(info.Name, "test"));
+
+            model.RegisterAllPorts();
+            model.NickName = "BHoM " + model.test.Name;
+            model.OverrideNameWithNickName = true;
+        }
+    }
+
+}
