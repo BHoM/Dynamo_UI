7e046580af4809ce7983aef859777d8b51d3d294
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2017-11-14 10:54:41 +0000

    Added Environmental Basilisk
    Create location, convert to BHoM and to DesignScript

---
+++ Environmental_Basilisk/Convert/ToBHoM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADG = Autodesk.DesignScript.Geometry;
+using BHG = BH.oM.Geometry;
+using BHE = BH.oM.Environmental;
+
+namespace Basilisk.Environmental
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BHE.Elements.Location ToBHoM(DynamoUnits.Location dynamoLocation)
+        {
+
+            string placename = dynamoLocation.Name;
+            double latitude = dynamoLocation.Latitude;
+            double longitude = dynamoLocation.Longitude;
+
+            return new BHE.Elements.Location(placename, latitude, longitude, 0);
+
+        }
+
+    }
+}
---
+++ Environmental_Basilisk/Convert/ToDesignScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADG = Autodesk.DesignScript.Geometry;
+using BHG = BH.oM.Geometry;
+using BHE = BH.oM.Environmental;
+
+namespace Basilisk.Environmental
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+
+        public static DynamoUnits.Location ToDesignScript(BHE.Elements.Location bhomlocation)
+        {
+
+            string placename = bhomlocation.Name;
+            double latitude = bhomlocation.Latitude;
+            double longitude = bhomlocation.Longitude;
+
+            return DynamoUnits.Location.ByLatitudeAndLongitude(latitude, longitude, placename);
+
+        }
+
+    }
+}
