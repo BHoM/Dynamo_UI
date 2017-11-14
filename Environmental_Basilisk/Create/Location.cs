b35183bb66c1d127776b40ae0d299f83bd08951a
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2017-11-14 10:54:41 +0000

    Added Environmental Basilisk
    Create location, convert to BHoM and to DesignScript

---
+++ Environmental_Basilisk/Create/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHE = BH.oM.Environmental;
+
+namespace Basilisk.Environmental
+{
+    public static partial class Create
+    {
+
+        public static BHE.Elements.Location Location(string name, double latitude, double longitude, double elevation)
+        {
+
+            return new BHE.Elements.Location(name, latitude, longitude, elevation);
+
+        }
+
+    }
+}
