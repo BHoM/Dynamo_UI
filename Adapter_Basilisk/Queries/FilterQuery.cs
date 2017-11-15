3abe84bb032afb8b07bd93d25ad4b74d0d0ddb6e
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2017-11-15 16:11:26 +0000

    Added CustomQuery

---
+++ Adapter_Basilisk/Queries/FilterQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.UI.Basilisk.Adapter
+{
+    public static partial class Query
+    {
+        public static BH.Adapter.Queries.CustomQuery CustomQuery(string Query = null)
+        {
+            return new BH.Adapter.Queries.CustomQuery(Query);
+        }
+    }
+}
