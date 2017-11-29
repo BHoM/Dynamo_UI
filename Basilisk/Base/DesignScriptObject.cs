1d6a888bffc8a809ece047246fd56b35ae5599e1
smalmste <smalmste@BTH-PC0EHE0Y>
2017-11-29 11:15:22 +0000

    Added DesignScriptObject
    To convert BHoM geometries to DesignScript geometries in Dynamo

---
+++ Basilisk/Base/DesignScriptObject.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.DesignScript.Runtime;
+using BH.Adapter;
+using BH.Engine.Base;
+using BH.Engine.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHB = BH.oM.Base;
+using BHG = BH.oM.Geometry;
+using ADG = Autodesk.DesignScript.Geometry;
+using DA = BH.Adapter.DesignScript;
+
+namespace Basilisk.Base
+{
+    public static class DesignScriptObject
+    {
+        public static ADG.Geometry DesignScriptGeometry(object bhElement)
+        {
+            return DA.Convert.IToDesignScript(bhElement as dynamic);
+        }   
+    }
+}
