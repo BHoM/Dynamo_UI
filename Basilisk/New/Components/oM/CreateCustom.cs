1fc790f78afbb5a5b93b0c8d5ef32fd8a440300e
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-10-15 14:16:22 +0800

    serialisation of components and small fixes

--- Basilisk/New/Components/oM/CreateCustom.cs
+++ Basilisk/New/Components/oM/CreateCustom.cs
@@ -2,9 +2,12 @@
 using BH.UI.Basilisk.Templates;
 using BH.UI.Components;
 using BH.UI.Templates;
+using Dynamo.Graph;
 using Dynamo.Graph.Nodes;
 using ProtoCore.AST.AssociativeAST;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 
 namespace BH.UI.Basilisk.Components
 {
@@ -19,6 +22,53 @@ namespace BH.UI.Basilisk.Components
         /*******************************************/
 
         public override Caller Caller { get; } = new CreateCustomCaller();
+
+
+        /*******************************************/
+        /**** Override Methods                  ****/
+        /*******************************************/
+
+        protected override void SerializeCore(XmlElement element, SaveContext context)
+        {
+            base.SerializeCore(element, context);
+            var xmlDoc = element.OwnerDocument;
+
+            var nameList = xmlDoc.CreateElement("InputNames");
+            element.AppendChild(nameList);
+            foreach (string input in Caller.InputParams.Select(x => x.Name))
+            {
+                var name = xmlDoc.CreateElement("InputName");
+                name.SetAttribute("value", input);
+                nameList.AppendChild(name);
+            }
+        }
+
+        /*******************************************/
+
+        protected override void DeserializeCore(XmlElement element, SaveContext context)
+        {
+            base.DeserializeCore(element, context);
+
+            base.DeserializeCore(element, context);
+
+            List<string> InputNames = new List<string>();
+
+            foreach (XmlNode node in element.ChildNodes)
+            {
+                if (node.Name == "InputNames")
+                {
+                    foreach (XmlNode child in node.ChildNodes)
+                    {
+                        if (child.Attributes != null && child.Attributes["value"] != null)
+                            InputNames.Add(child.Attributes["value"].Value);
+                    }
+                }
+            }
+
+            CreateCustomCaller caller = Caller as CreateCustomCaller;
+            caller.SetInputs(InputNames);
+            RegisterInputs();
+        }
 
 
         /*******************************************/
--- Basilisk/New/Templates/CallerValueList.cs
+++ Basilisk/New/Templates/CallerValueList.cs
@@ -13,6 +13,8 @@ using BH.Engine.UI;
 using System.Reflection;
 using System.Collections;
 using CoreNodeModels;
+using System.Xml;
+using Dynamo.Graph;
 
 namespace BH.UI.Basilisk.Templates
 {
@@ -85,6 +87,50 @@ namespace BH.UI.Basilisk.Templates
             return new List<AssociativeNode> { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functionCall) };
         }
 
+        /*******************************************/
+
+        protected override void SerializeCore(XmlElement element, SaveContext context)
+        {
+            base.SerializeCore(element, context);
+            var xmlDoc = element.OwnerDocument;
+
+            if (Caller.Selector != null)
+            {
+                var componentString = xmlDoc.CreateElement("Component");
+                componentString.SetAttribute("value", Caller.Selector.Write());
+                element.AppendChild(componentString);
+
+                var selectionIndex = xmlDoc.CreateElement("SelectionIndex");
+                selectionIndex.SetAttribute("value", SelectedIndex.ToString());
+                element.AppendChild(selectionIndex);
+            }
+        }
+
+        /*******************************************/
+
+        protected override void DeserializeCore(XmlElement element, SaveContext context)
+        {
+            base.DeserializeCore(element, context);
+
+            if (Caller.Selector != null)
+            {
+                foreach (XmlNode node in element.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "Component":
+                            if (node.Attributes != null && node.Attributes["value"] != null && node.Attributes["value"].Value != null)
+                                Caller.Selector.Read(node.Attributes["value"].Value);
+                            break;
+
+                        case "SelectionIndex":
+                            if (node.Attributes != null && node.Attributes["value"] != null && node.Attributes["value"].Value != null)
+                                SelectedIndex = int.Parse(node.Attributes["value"].Value);
+                            break;
+                    }
+                }
+            }
+        }
 
         /*******************************************/
         /**** Private Methods                   ****/
