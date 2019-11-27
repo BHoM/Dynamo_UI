7182bfc0f11cc9d99ae5acd7e1e82d6b929475a4
adecler <arnaud.declercq@burohappold.com>
2019-11-27 12:03:31 +0800

    remove legacy faulty code for the CreateCustom component

--- Dynamo_UI/Components/oM/CreateCustom.cs
+++ Dynamo_UI/Components/oM/CreateCustom.cs
@@ -47,52 +47,5 @@ namespace BH.UI.Dynamo.Components
 
 
         /*******************************************/
-        /**** Override Methods                  ****/
-        /*******************************************/
-
-        protected override void SerializeCore(XmlElement element, SaveContext context)
-        {
-            base.SerializeCore(element, context);
-            var xmlDoc = element.OwnerDocument;
-
-            var nameList = xmlDoc.CreateElement("InputNames");
-            element.AppendChild(nameList);
-            foreach (string input in Caller.InputParams.Select(x => x.Name))
-            {
-                var name = xmlDoc.CreateElement("InputName");
-                name.SetAttribute("value", input);
-                nameList.AppendChild(name);
-            }
-        }
-
-        /*******************************************/
-
-        protected override void DeserializeCore(XmlElement element, SaveContext context)
-        {
-            base.DeserializeCore(element, context);
-
-            base.DeserializeCore(element, context);
-
-            List<string> InputNames = new List<string>();
-
-            foreach (XmlNode node in element.ChildNodes)
-            {
-                if (node.Name == "InputNames")
-                {
-                    foreach (XmlNode child in node.ChildNodes)
-                    {
-                        if (child.Attributes != null && child.Attributes["value"] != null)
-                            InputNames.Add(child.Attributes["value"].Value);
-                    }
-                }
-            }
-
-            CreateCustomCaller caller = Caller as CreateCustomCaller;
-            caller.SetInputs(InputNames);
-            RegisterInputs();
-        }
-
-
-        /*******************************************/
     }
 }
